81df9e5f314fe43225778643996da99ebe0bc908
Bidrift <yassine.fighter@gmail.com>
2025-07-04 11:12:19 +0100

    Edit application to login/logout layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_04_100123) do
-  create_table "uses", force: :cascade do |t|
+ActiveRecord::Schema[8.0].define(version: 2025_07_04_101007) do
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -19,7 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_04_100123) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_uses_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_uses_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
